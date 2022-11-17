4d5bf441695729a39e24f009b331bbe212bf266f
Daniel Pearson <pearsondg@gmail.com>
2022-11-17 17:51:01 +0000

    lists working, bookmarks need work

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,19 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment )
+  end
+end
